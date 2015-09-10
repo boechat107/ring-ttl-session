e44dc2bbd8ec219b8ea16c914bc9bd12f1e1aa25
Andre Ambrosio Boechat <boechat107@gmail.com>
2015-09-10 20:39:12 -0300

    Test namespace for performance comparison

---
+++ test/ring_ttl_session/performance.clj
@@ -0,0 +1,48 @@
+(ns ring-ttl-session.performance
+  (:require [ring.middleware.session.store :refer :all]
+            [ring.middleware.session.memory :refer [memory-store]]
+            [ring-ttl-session.core :refer [ttl-memory-store]]
+            [criterium.core :refer [bench quick-bench]]))
+
+(defn check-nonexistent-read []
+  ;; Results:
+  ;; 
+  ;; Evaluation count : 2097924 in 6 samples of 349654 calls.
+  ;; Execution time mean : 276.700909 ns
+  ;; Execution time std-deviation : 4.436926 ns
+  ;; Execution time lower quantile : 270.910014 ns ( 2.5%)
+  ;; Execution time upper quantile : 282.034356 ns (97.5%)
+  ;; Overhead used : 13.799339 ns
+
+  ;; Evaluation count : 10700184 in 6 samples of 1783364 calls.
+  ;; Execution time mean : 44.712275 ns
+  ;; Execution time std-deviation : 2.904135 ns
+  ;; Execution time lower quantile : 42.764193 ns ( 2.5%)
+  ;; Execution time upper quantile : 49.585376 ns (97.5%)
+  ;; Overhead used : 13.799339 ns 
+  (let [ttl (ttl-memory-store 10)
+        mem (memory-store)]
+    (quick-bench (read-session ttl "nonexistent"))
+    (quick-bench (read-session mem "nonexistent"))))
+
+(defn check-session-create []
+  ;; Results:
+  ;; 
+  ;; Evaluation count : 324 in 6 samples of 54 calls.
+  ;; Execution time mean : 1.749771 ms
+  ;; Execution time std-deviation : 47.279785 µs
+  ;; Execution time lower quantile : 1.690888 ms ( 2.5%)
+  ;; Execution time upper quantile : 1.799542 ms (97.5%)
+  ;; Overhead used : 13.799339 ns
+
+  ;; Evaluation count : 104730 in 6 samples of 17455 calls.
+  ;; Execution time mean : 5.809839 µs
+  ;; Execution time std-deviation : 64.649296 ns
+  ;; Execution time lower quantile : 5.732449 µs ( 2.5%)
+  ;; Execution time upper quantile : 5.892600 µs (97.5%)
+  ;; Overhead used : 13.799339 ns
+  (let [ttl (ttl-memory-store 10)
+        mem (memory-store)
+        data {:foo "bar"}]
+    (quick-bench (write-session ttl nil data))
+    (quick-bench (write-session mem nil data))))
